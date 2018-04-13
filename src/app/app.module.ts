4e3c7f62c8a4c3aebc2461d42ca3e52b5c3817df
Mikhail Pushkin <lukashevichmikhail@gmail.com>
2018-04-13 13:07:17 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ClarityModule } from '@clr/angular';
+import { ROUTING } from "./app.routing";
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { HeaderComponent } from './common/header/header.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        AboutComponent,
+        HomeComponent,
+        HeaderComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ClarityModule,
+        ROUTING
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
